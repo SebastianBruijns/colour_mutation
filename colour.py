51371d959c86ee9e4428c9c289d359134ae41aa0
sbruijns <sebastian.bruijns@tuebingen.mpg.de>
2023-06-22 00:45:26 +0200

    fixed some issues, but weird colour is still there

--- colour.py
+++ colour.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-n, m = 450, 800
+n, m = 45, 80
 steps = [1, 10, 25, 40, 55, 70, 85]
 propagation_chance = 0.5
 step_size = 0.05
@@ -12,12 +12,15 @@ colours[n // 2, m // 2] = 0.5
 
 neigbhours = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
+saves = []
+
 for step in range(steps[-1]):
 	print(step)
-	xs, ys, _ = np.where(colours != 0)
+	xs, ys = np.where(colours[:, :, 0] != 0)
+	# saves.append(colours.copy())
 	for x, y in zip(xs, ys):
 		for neigh in neigbhours:
-			if 0 < x + neigh[0] < n and 0 < y + neigh[1] < m:
+			if 0 <= x + neigh[0] < n and 0 <= y + neigh[1] < m:
 				if (colours[x + neigh[0], y + neigh[1]] == np.zeros(3)).all():
 					if np.random.rand() < propagation_chance:
 						colours[x + neigh[0], y + neigh[1]] = colours[x, y] + np.random.rand(3) * step_size
