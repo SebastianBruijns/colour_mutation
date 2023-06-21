b3310808324325ba95c7c6ebd9b76c0d2bd24143
sbruijns <sebastian.bruijns@tuebingen.mpg.de>
2023-06-22 01:05:31 +0200

    fixed white background

--- colour.py
+++ colour.py
@@ -7,7 +7,7 @@ steps = [1, 10, 25, 40, 55, 70, 85]
 propagation_chance = 0.5
 step_size = 0.05
 
-colours = np.zeros((n, m, 3))
+colours = np.ones((n, m, 3))
 colours[n // 2, m // 2] = [0.1, 0.5, 0.3]
 
 neigbhours = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
@@ -16,15 +16,15 @@ saves = []
 
 for step in range(steps[-1]):
 	print(step)
-	xs, ys = np.where(colours[:, :, 0] != 0)
+	xs, ys = np.where(colours[:, :, 0] != 1)
 	# saves.append(colours.copy())
 	for x, y in zip(xs, ys):
 		for neigh in neigbhours:
 			if 0 <= x + neigh[0] < n and 0 <= y + neigh[1] < m:
-				if (colours[x + neigh[0], y + neigh[1]] == np.zeros(3)).all():
+				if (colours[x + neigh[0], y + neigh[1]] == np.ones(3)).all():
 					if np.random.rand() < propagation_chance:
 						colours[x + neigh[0], y + neigh[1]] = colours[x, y] + np.random.rand(3) * step_size - step_size / 2
-	colours = colours % 1
+	colours = np.clip(colours, 0, 1)
 	if step in steps:
 		plt.imshow(colours, vmin=0, vmax=1)
 		plt.savefig(str(step))
